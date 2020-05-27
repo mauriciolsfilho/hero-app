b0937d166ee7ae218b805e43a847de076a62494c
mauricio <mauricio_1souza@outlook.com>
2020-05-27 14:06:50 -0300

    Removendo validação

--- hero-api/src/controllers/ProfileController.js
+++ hero-api/src/controllers/ProfileController.js
@@ -1,12 +1,12 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async List(request, response) {
-        const ong_id = request.headers.authorization;
+    async List(req, resp) {
+        const ong_id = req.headers.authorization;
 
         const incidents = await connection('incidents')
             .where('ong_id', ong_id).select('*');
 
-        return response.json(incidents);
+        return resp.json(incidents);
     }
 }
--- hero-api/src/routes.js
+++ hero-api/src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { celebrate, Segments, Joi } = require('celebrate');
+//const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -9,19 +9,14 @@ const routes = express.Router();
 
     routes.post('/sessions', SessionController.Create);
 
+    //ONG'S
     routes.get('/ongs', OngController.List);
-    routes.post('/ongs', celebrate({
-        [Segments.BODY: Joi.object().keys({
-            name: Joi.string().required(),
-            email: Joi.string().required().email(),
-            whatsapp: Joi.number().required().min(10).max(11),
-            city: Joi.string(), required(),
-            uf: Joi.string().required().length(2)
-        })]
-    }), OngController.Create);
+    routes.post('/ongs', OngController.Create);
 
+    //PROFILE
     routes.get('/profile', ProfileController.List);
 
+    //INCIDENT
     routes.get('/incidents', IncidentController.List);
     routes.post('/incidents', IncidentController.Create);
     routes.delete('/incidents/:id', IncidentController.Remove);
