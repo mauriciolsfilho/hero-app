12e10c605a4a62cfb638304acce014cd27a02a30
mauricio <mauricio_1souza@outlook.com>
2020-05-27 12:22:43 -0300

    Validações com celebrate

--- hero-api/src/routes.js
+++ hero-api/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -9,7 +10,15 @@ const routes = express.Router();
     routes.post('/sessions', SessionController.Create);
 
     routes.get('/ongs', OngController.List);
-    routes.post('/ongs', OngController.Create);
+    routes.post('/ongs', celebrate({
+        [Segments.BODY: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required().min(10).max(11),
+            city: Joi.string(), required(),
+            uf: Joi.string().required().length(2)
+        })]
+    }), OngController.Create);
 
     routes.get('/profile', ProfileController.List);
 
